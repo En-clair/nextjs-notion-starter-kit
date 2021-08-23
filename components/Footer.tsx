05074af5568d4f60084f6cea911d0d0b2bfcc953
En-clair <signal.lepi@gmail.com>
2021-08-23 10:54:15 +0930

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2021, MIT Licence unless otherwise stated {config.author}</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
