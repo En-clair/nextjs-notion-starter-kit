d1c120c465391d7f9c1b6982159132b980ebc5cf
En-clair <signal.lepi@gmail.com>
2021-09-24 13:11:54 +0930

    Update PageHead.tsx
    Doesn't like the 'media' string/word in the meta tag. Something about typescript and nextjs not playing nice?

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -22,8 +22,7 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
         </>
       )}
 
-      <meta name="theme-color" content="#ecd96f" media="(prefers-color-scheme: light)"/>
-      <meta name="theme-color" content="#0b3e05" media="(prefers-color-scheme: dark)"/>
+      <meta name="theme-color" content="#0b3e05"/>
       <meta property='og:type' content='website' />
     </Head>
     </div>
